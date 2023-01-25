984d0ad3f0eae2eb342d7c195c5ae75817290ff5
Luyanda Lukhele <luylukhe021@student.wethinkcode.co.za>
2023-01-25 15:20:49 +0200

    Set up

---
+++ src/main/java/com/luyalukhele/WhatUPlayin/WhatUPlayinApplication.java
@@ -0,0 +1,13 @@
+package com.luyalukhele.WhatUPlayin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatUPlayinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatUPlayinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luyalukhele/WhatUPlayin/WhatUPlayinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luyalukhele.WhatUPlayin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatUPlayinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
